7c55859603c96b3c392f74e0c76ed72cd04ed602
Kenner Corps <kenner3.1@gmail.com>
2016-05-14 02:21:01 +0200

    Iniciando el sistema de multijugador.

--- server.js
+++ server.js
@@ -2,18 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var IDcount = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/main.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+	console.log('Jugador conectado!');
+
+	socket.on('new', function(){
+		io.emit('new', IDcount);
+		socket.broadcast.emit('add', IDcount);
+		IDcount++;
+	});
+
+	socket.on('pos', function(msg){
+		//console.log('pos: ' + msg);
+		socket.broadcast.emit('pos', msg);
+	});
+
+	socket.on('disconnect', function(){
+		console.log('Jugador desconectado.');
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
